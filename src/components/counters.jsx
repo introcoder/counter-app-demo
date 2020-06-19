13f8517ac4e3e3a8389245b1cb142eb803cf3f6c
Alex Ngo <alex.haianh@gmail.com>
2020-06-19 22:31:49 +0700

    single source of truth

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,6 +11,14 @@ class Counters extends Component {
     ],
   };
 
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
   handleDelete = (counterId) => {
     console.log("Event handler called", counterId);
     const counters = this.state.counters.filter((c) => c.id !== counterId);
@@ -20,6 +28,12 @@ class Counters extends Component {
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m2"
+        >
+          Reset
+        </button>
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
